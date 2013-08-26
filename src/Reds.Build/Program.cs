bfb4848ec72cfe2f12a89f053b75f316d8210163
nazirsj <nazirsj@gmail.com>
2013-08-26 10:17:22 +0200

    add hello world message

--- src/Reds.Build/Program.cs
+++ src/Reds.Build/Program.cs
@@ -10,6 +10,8 @@ namespace Reds.Build
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("Hello worlds");
+            Console.ReadLine();
         }
     }
 }
